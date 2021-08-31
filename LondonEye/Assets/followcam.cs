109d941098f296bc8c01acb84c60389a7965de45
aemeadow <60167778+aemeadow@users.noreply.github.com>
2021-08-31 13:31:01 -0400

    minor changes

--- LondonEye/Assets/followcam.cs
+++ LondonEye/Assets/followcam.cs
@@ -7,8 +7,11 @@ public class followcam : MonoBehaviour
 {
     public Transform Player;
     public float distanceFromObject = 3f;
+
+
     void FixedUpdate()
     {
+
         Vector3 lookOnObject = Player.position - transform.position;
         lookOnObject = Player.position - transform.position;
         transform.forward = lookOnObject.normalized;
@@ -16,6 +19,7 @@ public class followcam : MonoBehaviour
         playerLastPosition = Player.position - lookOnObject.normalized * distanceFromObject;
         playerLastPosition.y = Player.position.y + distanceFromObject / 2;
         transform.position = playerLastPosition;
+
     }
 
 }
