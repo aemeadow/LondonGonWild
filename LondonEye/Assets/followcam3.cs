257d69ead069031f9e6f1e9d090fb92bf60ef9bf
aemeadow <60167778+aemeadow@users.noreply.github.com>
2021-09-02 02:03:30 -0400

    smooth and adjustable angle follow cam

---
+++ LondonEye/Assets/followcam3.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+public class followcam3 : MonoBehaviour
+{
+
+    [Tooltip("Target to be followed")]
+    public Transform Target;
+    [Tooltip("Mimic the Target's changes in x coordinate")]
+    public bool FollowTargetX = true;
+    [Tooltip("Mimic the Target's changes in y coordinate")]
+    public bool FollowTargetY = true;
+    [Tooltip("Mimic the Target's changes in z coordinate")]
+    public bool FollowTargetZ = true;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Target)
+        {
+            Vector3 targetMovement = GetTargetMovement();
+            UpdatePosition(targetMovement);
+        }
+    }
+
+    private Vector3 oldTargetPosition;
+    private Vector3 GetTargetMovement()
+    {
+        if (oldTargetPosition == Vector3.zero)
+        {
+            oldTargetPosition = Target.transform.position;
+        }
+        Vector3 newTargetPosition = Target.transform.position;
+        Vector3 targetMovement = newTargetPosition - oldTargetPosition;
+        oldTargetPosition = new Vector3(newTargetPosition.x, newTargetPosition.y, newTargetPosition.z);
+        return targetMovement;
+    }
+
+    private void UpdatePosition(Vector3 targetMovement)
+    {
+        float xPosition = transform.position.x;
+        float yPosition = transform.position.y;
+        float zPosition = transform.position.z;
+        if (FollowTargetX)
+        {
+            xPosition += targetMovement.x;
+        }
+        if (FollowTargetY)
+        {
+            yPosition += targetMovement.y;
+        }
+        if (FollowTargetZ)
+        {
+            zPosition += targetMovement.z;
+        }
+        Vector3 updatedPosition = new Vector3(xPosition, yPosition, zPosition);
+        transform.position = updatedPosition;
+    }
+}
