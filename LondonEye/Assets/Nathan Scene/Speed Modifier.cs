8c1ba3367f6296faeaec1237cb6648acffd900a3
BoxingCactus <54558730+BoxingCactus@users.noreply.github.com>
2021-09-02 12:18:29 -0400

    Added the framework and debugging check for speed changes
    Made the outline for the speed changing script with some code to check if the colliders are working or not.

---
+++ LondonEye/Assets/Nathan Scene/Speed Modifier.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedModifier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Accelerator")) 
+        {
+            Debug.Log("Touched Speed");
+        }
+        if (other.gameObject.CompareTag("Decelerator")) 
+        {
+            Debug.Log("Touched Slow");
+        }
+    }
+}
