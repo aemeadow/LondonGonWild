8f3b9b414b3a80bfacf5b6d8bba8ee8def707b3e
BoxingCactus <54558730+BoxingCactus@users.noreply.github.com>
2021-09-03 16:36:57 -0400

    Added a score counter
    Added a score counter that uses TextMeshPro that tracks Accelerators and Decelerators.

--- LondonEye/Assets/Nathan Scene/SpeedModifier.cs
+++ LondonEye/Assets/Nathan Scene/SpeedModifier.cs
@@ -1,28 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpeedModifier : MonoBehaviour
 {
     public Rigidbody rb;
+    public TextMeshProUGUI Score;
+    public static int count = 0;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Score = GetComponent<TextMeshProUGUI>();
     }
-
-    // Update is called once per frame
+    void Update()
+    {
+        Score.text = "Score:" + count;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Accelerator")) 
         {
             rb.velocity = rb.velocity*1.2f;
             Debug.Log("Touched Speed");
+            count = count + 1;
         }
         if (other.gameObject.CompareTag("Decelerator")) 
         {
             rb.velocity = rb.velocity/2;
             Debug.Log("Touched Slow");
+            count = count - 1;
         }
     }
 }
